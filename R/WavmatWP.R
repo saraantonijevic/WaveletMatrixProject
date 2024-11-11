51ff586405c1696cc4c406a4c8164f3c87eacf4a
Sara Antonijevic <saraant@tamu.edu>
2024-11-11 10:40:06 -0600

    Making roxygen2 for new function WavmatWP.R

---
+++ R/WavmatWP.R
@@ -0,0 +1,5 @@
+#' Construct Wavelet Packet Transformation Matrix
+#'
+#' This function creates a wavelet packet transformation matrix for high-quality filter coefficients.
+#' It uses orthogonal wavelet transformation to construct the matrix.
+#'
