47bbaae579ada097365e14f4f751f275ce81f62e
Sara Antonijevic <saraant@tamu.edu>
2024-11-11 11:08:53 -0600

    applying WavMatPackWP to the data to test function.
    Printing out test to ensure its working properly

--- demo/WavPackWP_demo1.R
+++ demo/WavPackWP_demo1.R
@@ -14,3 +14,15 @@ shift <- 3   # standard shift
 filt <- c(sqrt(2)/2, sqrt(2)/2)
 
 
+# SYMMLET 4
+filt <- c(-0.07576571478934, -0.02963552764595,
+          0.49761866763246, 0.80373875180522,
+          0.29785779560554, -0.09921954357694,
+          -0.01260396726226, 0.03222310060407)
+
+WP <- WavmatWP(filt, n, level, shift) # size: [level*n, n]
+
+
+cat("Trace of WP * WP':", sum(diag(WP %*% t(WP))), "\n")  # should be n. level*n entries on the diagonal of size 1/level.
+cat("Trace of WP' * WP:", sum(diag(t(WP) %*% WP)), "\n")  # should be n. n entries of 1 each.
+
