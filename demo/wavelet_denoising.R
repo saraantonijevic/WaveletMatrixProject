f945f9a0fdca66db8b204ba7483946df8e148a6e
Sara Antonijevic <saraant@tamu.edu>
2024-11-10 21:58:42 -0600

    adding libraries for new demo for wavelet denoising

---
+++ demo/wavelet_denoising.R
@@ -0,0 +1,10 @@
+#' Demo for Wavelet Denoising using the WaveletMatrixProject package
+#'
+#' This demo showcases how to denoise a synthetic "bumps" signal using a wavelet
+#' packet transformation matrix constructed by WavPackMat.
+#' Run this demo using: demo("wavelet_denoising", package = "WaveletMatrixProject")
+
+# Load necessary libraries
+library(Matrix)
+library(ggplot2)
+library(WaveletMatrixProject)
