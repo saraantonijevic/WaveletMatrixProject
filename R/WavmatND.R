784bdd26fda3b8b1d4de9b7ea1cb3f4abed0e362
Sara Antonijevic <saraant@tamu.edu>
2024-11-19 15:19:45 -0600

    Adding roxygen for the functions.

--- R/WavmatND.R
+++ R/WavmatND.R
@@ -1,50 +1,21 @@
-#' Wavelet Matrix Construction and Utility Functions
+#' Wavelet Matrix Construction
 #'
-#' This set of functions includes tools for constructing wavelet matrices, dilating filters, embedding vectors, and repeating vectors for use in signal processing and wavelet transformations.
+#' Constructs a wavelet matrix using a high-pass filter, specified parameters, and an iterative process to build matrices.
 #'
-#' @param hf A vector representing the high-pass filter, used as input for constructing the wavelet matrix.
+#' @param hf A vector representing the high-pass filter.
 #' @param N An integer specifying the size of the wavelet matrix.
-#' @param k An integer indicating the number of iterations for constructing the wavelet matrix.
-#' @param shift An integer representing the shift to be applied in the wavelet matrix construction.
-#' @param filt A vector representing the filter to be dilated in the \code{dilate_filter} function.
-#' @param a A vector to be embedded or repeated, used in the \code{imbed} and \code{repeating} functions.
-#' @param n An integer specifying the number of times to repeat the vector in the \code{repeating} function.
-#'
-#' @details
-#' \strong{WavmatND:} Constructs a wavelet matrix using a high-pass filter, specified parameters, and an iterative process to build matrices with modulated entries based on circular indexing. It returns a complete wavelet matrix that can be used in signal processing.
-#'
-#' \strong{dilate_filter:} Inserts zeros between the elements of the input filter to dilate it, extending its length as required by wavelet transformations.
-#'
-#' \strong{imbed:} Embeds a vector by appending zeros to double its length.
-#'
-#' \strong{repeating:} Repeats the input vector a specified number of times.
-#'
-#' @return
-#' \itemize{
-#'   \item \code{WavmatND}: A matrix representing the wavelet transformation.
-#'   \item \code{dilate_filter}: A vector of the dilated filter.
-#'   \item \code{imbed}: A vector with zeros appended to double its length.
-#'   \item \code{repeating}: A vector with repeated elements.
-#' }
-#'
+#' @param k An integer indicating the number of iterations.
+#' @param shift An integer representing the shift to be applied.
+#' @return A matrix representing the wavelet transformation.
 #' @examples
 #' hf <- c(0.5, -0.5)
 #' N <- 4
 #' k <- 2
 #' shift <- 1
 #' W <- WavmatND(hf, N, k, shift)
-#'
-#' filt <- c(0.5, -0.5)
-#' dilated_filt <- dilate_filter(filt, 2)
-#'
-#' a <- c(1, 2, 3)
-#' imbedded_a <- imbed(a)
-#'
-#' a <- c(1, 2)
-#' repeated_a <- repeating(a, 3)
-#'
 #' @export
-#' @importFrom Matrix rBind, Diagonal
+
+#' @importFrom Matrix Diagonal
 #' @import ggplot2
 #' @import imager
 #' @import pracma
@@ -81,6 +52,18 @@ WavmatND <- function(hf, N, k, shift) {
   return(W)
 }
 
+
+#' Dilate Filter
+#'
+#' Inserts zeros between the elements of the input filter to dilate it.
+#'
+#' @param filt A vector representing the filter to be dilated.
+#' @param k An integer specifying the step size for dilation.
+#' @return A vector of the dilated filter.
+#' @examples
+#' filt <- c(0.5, -0.5)
+#' dilated_filt <- dilate_filter(filt, 2)
+#' @export
 dilate_filter <- function(filt, k) {
   newlength <- (k + 1) * length(filt) - k
   filtd <- rep(0, newlength)
@@ -88,11 +71,34 @@ dilate_filter <- function(filt, k) {
   return(filtd)
 }
 
+
+#' Imbed Vector
+#'
+#' Embeds a vector by appending zeros to double its length.
+#'
+#' @param a A vector to be embedded.
+#' @return A vector with zeros appended to double its length.
+#' @examples
+#' a <- c(1, 2, 3)
+#' imbedded_a <- imbed(a)
+#' @export
 imbed <- function(a) {
   b <- c(a, rep(0, length(a)))
   return(b)
 }
 
+
+#' Repeat Vector
+#'
+#' Repeats the input vector a specified number of times.
+#'
+#' @param a A vector to be repeated.
+#' @param n An integer specifying the number of times to repeat the vector.
+#' @return A vector with repeated elements.
+#' @examples
+#' a <- c(1, 2)
+#' repeated_a <- repeating(a, 3)
+#' @export
 repeating <- function(a, n) {
   b <- rep(a, n)
   return(b)
