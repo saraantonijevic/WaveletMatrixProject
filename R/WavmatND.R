bc7ab15a12dc5d1a10e376576ca551488fa52eb1
Sara Antonijevic <saraant@tamu.edu>
2024-11-18 19:55:35 -0600

    adding roxygen

--- R/WavmatND.R
+++ R/WavmatND.R
@@ -1,5 +1,54 @@
+#' Wavelet Matrix Construction and Utility Functions
+#'
+#' This set of functions includes tools for constructing wavelet matrices, dilating filters, embedding vectors, and repeating vectors for use in signal processing and wavelet transformations.
+#'
+#' @param hf A vector representing the high-pass filter, used as input for constructing the wavelet matrix.
+#' @param N An integer specifying the size of the wavelet matrix.
+#' @param k An integer indicating the number of iterations for constructing the wavelet matrix.
+#' @param shift An integer representing the shift to be applied in the wavelet matrix construction.
+#' @param filt A vector representing the filter to be dilated in the \code{dilate_filter} function.
+#' @param a A vector to be embedded or repeated, used in the \code{imbed} and \code{repeating} functions.
+#' @param n An integer specifying the number of times to repeat the vector in the \code{repeating} function.
+#'
+#' @details
+#' \strong{WavmatND:} Constructs a wavelet matrix using a high-pass filter, specified parameters, and an iterative process to build matrices with modulated entries based on circular indexing. It returns a complete wavelet matrix that can be used in signal processing.
+#'
+#' \strong{dilate_filter:} Inserts zeros between the elements of the input filter to dilate it, extending its length as required by wavelet transformations.
+#'
+#' \strong{imbed:} Embeds a vector by appending zeros to double its length.
+#'
+#' \strong{repeating:} Repeats the input vector a specified number of times.
+#'
+#' @return
+#' \itemize{
+#'   \item \code{WavmatND}: A matrix representing the wavelet transformation.
+#'   \item \code{dilate_filter}: A vector of the dilated filter.
+#'   \item \code{imbed}: A vector with zeros appended to double its length.
+#'   \item \code{repeating}: A vector with repeated elements.
+#' }
+#'
+#' @examples
+#' hf <- c(0.5, -0.5)
+#' N <- 4
+#' k <- 2
+#' shift <- 1
+#' W <- WavmatND(hf, N, k, shift)
+#'
+#' filt <- c(0.5, -0.5)
+#' dilated_filt <- dilate_filter(filt, 2)
+#'
+#' a <- c(1, 2, 3)
+#' imbedded_a <- imbed(a)
+#'
+#' a <- c(1, 2)
+#' repeated_a <- repeating(a, 3)
+#'
+#' @export
+#' @importFrom Matrix rBind
+#' @import ggplot2
+#' @import imager
+#' @import pracma
 WavmatND <- function(hf, N, k, shift) {
-  # hf = as.vector(hf) # Not needed in R as it is already a vector
   gf <- rev(Conj(hf) * (-1)^(1:length(hf)))
   W <- matrix(nrow = 0, ncol = N)
   hmatold <- diag(N)
@@ -42,8 +91,7 @@ imbed <- function(a) {
   return(b)
 }
 
-repeat <- function(a, n) {
+repeating <- function(a, n) {
   b <- rep(a, n)
   return(b)
 }
-
