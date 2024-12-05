4b6afb03322f84c718639c55be40a80bfd361207
Sara Antonijevic <saraant@tamu.edu>
2024-12-05 08:21:45 -0600

    Instead of having them as separate functions, I wanted to keep the code more in line to the functions of WavmatWP.R and Wavmat.R so I integrated dilated_filt, imbed, and repeating into WavmatND.R.

--- R/WavmatND.R
+++ R/WavmatND.R
@@ -14,20 +14,22 @@
 #' shift <- 1
 #' W <- WavmatND(hf, N, k, shift)
 #' @export
-
 #' @importFrom Matrix Diagonal
 #' @import ggplot2
 #' @import imager
 #' @import pracma
 #' @import ggplot2
 #' @import gridExtra
+
 WavmatND <- function(hf, N, k, shift) {
+  # Generate the conjugate high-pass filter
   gf <- rev(Conj(hf) * (-1)^(1:length(hf)))
   W <- matrix(nrow = 0, ncol = N)
   hmatold <- diag(N)
   h <- c(hf, rep(0, N))
   g <- c(gf, rep(0, N))
 
+  # Iterative process to construct the wavelet matrix
   for (i in 1:k) {
     gmat <- matrix(0, nrow = N, ncol = N)
     hmat <- matrix(0, nrow = N, ncol = N)
@@ -52,57 +54,21 @@ WavmatND <- function(hf, N, k, shift) {
   return(W)
 }
 
-
-#' Dilate Filter
-#'
-#' Inserts zeros between the elements of the input filter to dilate it.
-#'
-#' @param filt A vector representing the filter to be dilated.
-#' @param k An integer specifying the step size for dilation.
-#' @return A vector of the dilated filter.
-#' @examples
-#' filt <- c(0.5, -0.5)
-#' dilated_filt <- dilate_filter(filt, 2)
-#' @export
 dilate_filter <- function(filt, k) {
   if (length(filt) == 0) {
     return(numeric(0))
   }
-
   newlength <- (k + 1) * length(filt) - k
   filtd <- rep(0, newlength)
   filtd[seq(1, newlength, by = k + 1)] <- filt
   return(filtd)
 }
 
-
-#' Imbed Vector
-#'
-#' Embeds a vector by appending zeros to double its length.
-#'
-#' @param a A vector to be embedded.
-#' @return A vector with zeros appended to double its length.
-#' @examples
-#' a <- c(1, 2, 3)
-#' imbedded_a <- imbed(a)
-#' @export
 imbed <- function(a) {
   b <- c(a, rep(0, length(a)))
   return(b)
 }
 
-
-#' Repeat Vector
-#'
-#' Repeats the input vector a specified number of times.
-#'
-#' @param a A vector to be repeated.
-#' @param n An integer specifying the number of times to repeat the vector.
-#' @return A vector with repeated elements.
-#' @examples
-#' a <- c(1, 2)
-#' repeated_a <- repeating(a, 3)
-#' @export
 repeating <- function(a, n) {
   b <- rep(a, n)
   return(b)
